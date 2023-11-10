a2226302e75fad48005b85f3245dd4b546d6c450
Ana Luisa Cardoso <106750168+AnaLuisaCardoso007@users.noreply.github.com>
2023-11-10 09:05:59 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,40 @@
-function tocaSomPom(){
+function tocaSom(seletorAudio){
+ const elemento = document.querySelector(seletorAudio).play('');
 
-    document.querySelector('#som_tecla_pom').play('');}
-document.querySelector('.tecla_pom').onclick = tocaSomPom ();
+if(elemento === null){
+alert('Elemento não encontrado');
+}
+if(elemento != null){
+    elemento.play();
+}
+}
+ 
+    const listaDeTeclas = document.querySelectorAll('.tecla');
+
+   for(contador = 0; contador < listaDeTeclas.length; contador++){
+
+const instrumento = listaDeTeclas[contador].classList[1];
+
+   const tecla = listaDeTeclas[contador];
+
+  const idAudio = '#som_${instrumento}';
+   
+  tecla.onclick = function(){
+    tocaSom(idAudio);
+};
+
+tecla.onkeydown = function (evento)
+   if(evento.code === 'Space' || evento.code === 'Enter'){
+    tecla.classList.add('ativa');
+}
+tecla.onkeyup = function(evento){
+    tecla.classList.remove('ativa');
+}
+   
+listaDeTeclas[1].onclick = tocaSomClap;
+listaDeTeclas[2].onclick = tocaSomTim;
+listaDeTeclas[3].onclick = tocaSomPuff;
+
+   document.querySelector('.tecla_pom').onclick = tocaSomPom ();
+
+    //document.querySelector('.tecla_pom').onclick = toca
